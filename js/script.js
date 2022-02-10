c6873a9d11e507af39701a6df4edf6fdb38bc910
RedS0ul12 <futza06@gmail.com>
2022-02-10 01:05:12 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+import { data } from '/.data.js'
+
+const main = document.querySelector('#main')
+
+function showHeroes(heroes) {
+
+    main.innerHTML = '';
+
+    heroes.forEach((heroe)=> {
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${heroe.image}" alt="${heroe.name}">
+            <div class="movie-info">
+                <h3>${heroe.name}</h3>
+                <span class="green">0.9</span>
+            </div>
+            <div class="overview">
+                <h3>overview</h3>
+                ${heroe.first_appearance}
+            </div>
+        `
+
+        main.appendChild(movieEl)
+    })
+}
+
+showHeroes(data);
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const search = document.querySelector('#search').value;
+
+    const resultado = data.filter(heroe=> heroe.superheroe.toLocaleLowerCase().incluides(search.toLocaleLowerCase()) || heroe.name.toLocaleLowerCase().incluides(search.toLocaleLowerCase()))
+
+    showHeroes(resultado);
+})
